f7a01ec9ba2ae48240c3c99f71b71d5f27f649bc
Nilam Doctor <nilamdoc@gmail.com>
2019-08-10 15:57:20 +0530

    SSL

--- app/webroot/en/index.php
+++ app/webroot/en/index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <?php 
-	// if (!isset($_SERVER['HTTPS'])) {
-	// header('Location: https://' . $_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']);
-	// exit;
-// }
+	if (!isset($_SERVER['HTTPS'])) {
+	header('Location: https://' . $_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']);
+	exit;
+}
 ?>
 <html lang="en">
 <head>
